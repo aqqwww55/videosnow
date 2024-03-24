66115652ee432c38a963d5e4b22898e3d6d94869
sapna Videos <85022892+aqqwww55@users.noreply.github.com>
2024-03-24 20:10:23 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+'script':
+{
+   "dev": "next dev",
+             ==>  "build": "next build", 
+    "start": "next start",
+    "lint": "next lint"
 }
-
-module.exports = nextConfig
